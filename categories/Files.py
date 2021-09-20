b89279502c6d4be8f651ff7b16bfa7022d4c830c
Tyler Clemens <tclemenade@gmail.com>
2021-09-20 13:34:31 -0500

    A small update to file upload complete and error messages.

--- categories/Files.py
+++ categories/Files.py
@@ -58,11 +58,11 @@ class Files(TapipyCategory):
                 source_file_path = path_to_file,
                 dest_file_path = destination_folder
             )
-
-            self.logger.complete(f"Uploaded file '{path_to_file}' to {destination_folder}")
-            
+            print()
+            self.logger.complete(f"Uploaded file '{path_to_file}' to {destination_folder}\n")
+            return
         except Exception as e:
-            self.logger.error(e.message)
+            self.logger.error(f"{e.message}\n")
             self.exit(1)
 
     def upload_folder(self, system_id, path_to_folder, destination_folder) -> None:
@@ -82,6 +82,6 @@ class Files(TapipyCategory):
                 )
                 self.logger.complete(f"Successfully uploaded {file} to {os.path.join(destination_folder, file)}")
             except Exception as e:
-                self.logger.error(e.message)
+                self.logger.error(f"{e.message}\n")
                 self.exit(1)
         self.logger.success(f"Successfully uploaded files to {destination_folder}\n")
